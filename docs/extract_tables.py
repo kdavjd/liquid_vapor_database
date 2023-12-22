d4e53851e6ca56dae6d3538e03fca990ffd39cfc
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-12-22 12:07:57 +0300

    poetry added

--- docs/extract_tables.py
+++ docs/extract_tables.py
@@ -6,7 +6,7 @@ from src.pdf_page_processor import Page
 
 if __name__ == '__main__':
     # Путь к PDF файлу и путь к Tesseract
-    NUM_PAGE = 132
+    NUM_PAGE = 134
     pdf_path = 'C:/IDE/repository/liquid_vapor_database/data/Kogan_1.pdf'
     tesseract_path = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
     image = PDFImageExtractor(pdf_path, tesseract_path, images_folder=f'pdf_data/page_{NUM_PAGE}')        
